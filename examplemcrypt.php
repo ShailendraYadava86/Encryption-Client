4851274d5b7dd035de2561f83dbb0be884f39a78
shailendrayadava86 <49182874+shailendrayadava86@users.noreply.github.com>
2019-04-04 17:06:19 +0530

    Update
    Update

--- examplemcrypt.php
+++ examplemcrypt.php
@@ -1,12 +1,12 @@
 <?php
 
-use Shai\Encrypt\EncriptData;
-include(dirname(__FILE__).'/Encryption-Client/src/Encrypt.php');
+use StringEncrypt\Encrypt;
+require (dirname(__FILE__) . '/vendor/autoload.php');
 // Pass String to Encrypt
-echo $encryptionout=EncriptData::mcryptEncript('Toooo Use Change');
+echo $encryptionout=Encrypt::mcryptEncript('Toooo Use Change');
 echo "<br/>";
 // Pass Encrypted string to find original string
-echo EncriptData::mcryptDecript($encryptionout)."<br/>";
+echo Encrypt::mcryptDecript($encryptionout)."<br/>";
 
 
 
