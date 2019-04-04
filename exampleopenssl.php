81c1e99d8939a07cec68886f9744039d1b06b18b
shailendrayadava86 <49182874+shailendrayadava86@users.noreply.github.com>
2019-04-04 17:03:37 +0530

    Update
    Update

--- exampleopenssl.php
+++ exampleopenssl.php
@@ -1,11 +1,11 @@
 <?php
 
-use Shai\Encrypt\EncriptData;
+use StringEncrypt\Encrypt;
 include(dirname(__FILE__).'/Encryption-Client/src/Encrypt.php');
 // Pass Any String to Encrypt
-echo EncriptData::opensslEncript('Toooo Use Change')."<br/>";
+echo Encrypt::opensslEncript('Toooo Use Change')."<br/>";
 // Pass Encrypted string to find original string
-echo EncriptData::opensslDecript('cENHNEh0ZVdvbDZXeUJ1N2wyUmp6OEk5aUI2UTloWGErOVZXaU9RZDhIYz0=');
+echo Encrypt::opensslDecript('cENHNEh0ZVdvbDZXeUJ1N2wyUmp6OEk5aUI2UTloWGErOVZXaU9RZDhIYz0=');
 
  
  
