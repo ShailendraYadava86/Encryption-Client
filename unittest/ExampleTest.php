6e6acdae10229b3fda92b87ab02ebcab7fc4a04c
shailendrayadava86 <49182874+shailendrayadava86@users.noreply.github.com>
2019-04-07 10:29:11 +0530

    Update TestCase
    Update TestCase

--- unittest/ExampleTest.php
+++ unittest/ExampleTest.php
@@ -6,20 +6,39 @@ use StringEncrypt\Encrypt;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic functional test Openssl example.
+     * For Sppecific method Test like OpenSSL Encrypt and Decrypt
+     * @param string $originalString String 
+     * @param string $expectedResult
      *
      * @return string  
 
      */ 
 
-     public function testOpenssl(){
-        $expected_result = true;
-        $actual_result = Encrypt::opensslEncript('PhpUnit tested String Encryption and Decryption');
-        $actual_resultout = Encrypt::opensslDecript($actual_result);
-        echo 'Encrypted String is '.$actual_result;
-        echo 'Decrypted String is '.$actual_resultout;
+    public function testOpenssl(){
+     	$originalString='PhpUnit tested String Encryption and Decryption';
+     	$expectedResult='dUpqdWk3K1BKc0J5eFY2bDBDOGZuV0pNUlIzdG1yMGhnb1pEV0o3NFJEU2cwcDlpVUhxKzZJdVRIRU1PMmVIYg==';
+        $actualResult = Encrypt::opensslEncript($originalString);
+        $this->assertEquals($expectedResult,$actualResult);
     }
 
+    public function testOpensslDecrypt(){
+     	$originalString='dUpqdWk3K1BKc0J5eFY2bDBDOGZuV0pNUlIzdG1yMGhnb1pEV0o3NFJEU2cwcDlpVUhxKzZJdVRIRU1PMmVIYg==';	
+     	$expectedResult='PhpUnit tested String Encryption and Decryption';
+        $actualResult = Encrypt::opensslDecript($originalString);
+        $this->assertEquals($expectedResult,$actualResult);
+    }
+    
+    public function testOpensslEncryptForEmptyString(){
+     	$originalString='';	
+     	$expectedResult='MzVoNnc0TEd1cVVwcmdWRTgrVno2UT09';
+        $actualResult = Encrypt::opensslEncript($originalString);
+        $this->assertEquals($expectedResult,$actualResult);
+    }      
 
+    public function testOpensslDecryptForEmptyString(){
+     	$originalString='MzVoNnc0TEd1cVVwcmdWRTgrVno2UT09';	
+     	$expectedResult='';
+        $actualResult = Encrypt::opensslDecript($originalString);
+        $this->assertEquals($expectedResult,$actualResult);
+    }
 }
-
